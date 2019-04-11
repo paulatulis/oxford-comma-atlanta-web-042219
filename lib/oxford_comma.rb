e8403d28c35207a3ee7266d1fd06d8f66bd924f1
Paula Tulis <tulis.paula@gmail.com>
2019-04-11 20:50:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,15 @@ array = ["paula","richard", "michelle", "martin"]
 
 
 def oxford_comma(array)
-last_element = "and #{array.last}"
+if array.length == 1 
+  array.join 
+  elsif array.length == 2 
+  array.join("and")
+else 
+  last_element = "and #{array.last}"
 array.pop 
 array.join(",") 
 array << last_element
 array.join(', ') 
 end
+end 
