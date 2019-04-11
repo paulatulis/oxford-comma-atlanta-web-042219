3007c844aeb57a51a77e545f9c9ed4bb8d5c2999
Paula Tulis <tulis.paula@gmail.com>
2019-04-11 20:33:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,11 @@ array = ["paula","richard", "michelle", "martin"]
 
 
 def oxford_comma(array)
+last_item = []
+last_item = array.pop
+array << "and"
+array
+array << last_item 
 array.join(", ")
+array.delete_at(-2)
 end
