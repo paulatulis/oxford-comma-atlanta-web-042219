2b19d32fb2dacf3848930e1b598f1dd66475d7cb
Paula Tulis <tulis.paula@gmail.com>
2019-04-11 20:50:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+array = ["paula","richard", "michelle", "martin"]
 
+
+def oxford_comma(array)
+if array.length == 1 
+  array.join 
+  elsif array.length == 2 
+  array.join(" and ")
+else 
+  last_element = "and #{array.last}"
+array.pop 
+array.join(",") 
+array << last_element
+array.join(', ') 
 end
+end 
